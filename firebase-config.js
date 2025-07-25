28ac5e4c4cae207f862ece61c941c43ab34fe0ec
FeyoMx <FeyoMx@users.noreply.github.com>
2025-07-25 16:46:51 +0000

    deploy: d4466438e3b9160e1b6cd6224298a21861c27f97

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  apiKey: '',
-  authDomain: '',
-  projectId: '',
-  storageBucket: '',
-  messagingSenderId: '',
-  appId: ''
+  apiKey: 'AIzaSyA-Ky-4mk7Whzd0xvA8KzJKC94H2rVLX_o',
+  authDomain: 'menu-capibobba.firebaseapp.com',
+  projectId: 'menu-capibobba',
+  storageBucket: 'menu-capibobba.firebasestorage.app',
+  messagingSenderId: '899370367978',
+  appId: '1:899370367978:web:2143034b34964d6266d093'
 };
